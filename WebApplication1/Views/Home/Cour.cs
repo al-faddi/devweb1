cd3e78c0c3b85ff84a695470ebd4c170f14a14c5
faddi <franckamari@gmail.com>
2022-08-05 14:57:17 +0000

    test

---
+++ WebApplication1/Views/Home/Cour.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Views.Home
+{
+    public class Cour
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public ICollection<Eleve> Eleves { get; set; }
+
+        //Gerer les packages nuget sur outils- gestionnaire de package nuget et le 2ieme
+    }
+}
